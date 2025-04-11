19ffaf004c7d6b8073d3c6c0a7c42f306f530017
LouisAsanaka <25756888+LouisAsanaka@users.noreply.github.com>
2025-04-11 01:53:11 -0500

    Refactored loading code for columnar data structure

--- src/execute.cpp
+++ src/execute.cpp
@@ -1,23 +1,27 @@
 #include <iostream>
 #include <hardware.h>
+#include <memory>
 #include <plan.h>
 #include <table.h>
 #include <bloom_filter.h>
 #include <column_iterator.h>
 #include <simple_columnar_table.h>
 #include <variant>
+#include <ranges>
 
 namespace Contest {
 
-using ExecuteResult = std::vector<std::vector<Data>>;
+struct WorkContext {
+    std::vector<std::shared_ptr<SimpleColumnarTable>> tables;
+};
 
-SimpleColumnarTable execute_impl(const Plan& plan, size_t node_idx);
+SimpleColumnarTableView execute_impl(const Plan& plan, WorkContext* context, size_t node_idx);
 
 struct JoinAlgorithm {
     bool                                             build_left;
-    SimpleColumnarTable&                             left;
-    SimpleColumnarTable&                             right;
-    SimpleColumnarTable&                             results;
+    SimpleColumnarTableView&                         left;
+    SimpleColumnarTableView&                         right;
+    SimpleColumnarTable                              results;
     size_t                                           left_col, right_col;
     const std::vector<std::tuple<size_t, DataType>>& output_attrs;
 
@@ -41,6 +45,7 @@ struct JoinAlgorithm {
         if (build_left) {
             hash_table.reserve(left.row_count());
             bloom_filter.init(left.row_count(), 0.01);
+
             // Build hash table from left table
             const auto& left_column = left.get_column(left_col).values;
             for (size_t idx = 0; idx < left.row_count(); idx++) {
@@ -50,6 +55,7 @@ struct JoinAlgorithm {
                         hash_table[key].push_back(idx);
                         bloom_filter.insert(key);
                     } else if constexpr (not std::is_same_v<Tk, std::monostate>) {
+                        std::cout << "left hash build k = " << key << std::endl;
                         throw std::runtime_error("wrong type of field");
                     }
                 }, left_column[idx]);
@@ -82,6 +88,7 @@ struct JoinAlgorithm {
                             }
                         }
                     } else if constexpr (not std::is_same_v<Tk, std::monostate>) {
+                        std::cout << "right probe k = " << key << std::endl;
                         throw std::runtime_error("wrong type of field");
                     }
                 }, right_column[right_idx]);
@@ -90,6 +97,7 @@ struct JoinAlgorithm {
         } else {
             hash_table.reserve(right.row_count());
             bloom_filter.init(right.row_count(), 0.01);
+
             // Build hash table from right table
             const auto& right_column = right.get_column(right_col).values;
             for (size_t idx = 0; idx < right.row_count(); idx++) {
@@ -99,6 +107,7 @@ struct JoinAlgorithm {
                         hash_table[key].push_back(idx);
                         bloom_filter.insert(key);
                     } else if constexpr (not std::is_same_v<Tk, std::monostate>) {
+                        std::cout << "right hash k = " << key << std::endl;
                         throw std::runtime_error("wrong type of field");
                     }
                 }, right_column[idx]);
@@ -130,6 +139,7 @@ struct JoinAlgorithm {
                             }
                         }
                     } else if constexpr (not std::is_same_v<Tk, std::monostate>) {
+                        std::cout << "left probe k = " << key << std::endl;
                         throw std::runtime_error("wrong type of field");
                     }
                 }, left_column[left_idx]);
@@ -139,7 +149,8 @@ struct JoinAlgorithm {
     }
 };
 
-SimpleColumnarTable execute_hash_join(const Plan&          plan,
+SimpleColumnarTableView execute_hash_join(const Plan&    plan,
+    WorkContext*                                     context,
     const JoinNode&                                  join,
     const std::vector<std::tuple<size_t, DataType>>& output_attrs) {
     auto                           left_idx    = join.left;
@@ -148,14 +159,13 @@ SimpleColumnarTable execute_hash_join(const Plan&          plan,
     auto&                          right_node  = plan.nodes[right_idx];
     auto&                          left_types  = left_node.output_attrs;
     auto&                          right_types = right_node.output_attrs;
-    auto                           left        = execute_impl(plan, left_idx);
-    auto                           right       = execute_impl(plan, right_idx);
-    SimpleColumnarTable results;
+    auto                           left        = execute_impl(plan, context, left_idx);
+    auto                           right       = execute_impl(plan, context, right_idx);
 
     JoinAlgorithm join_algorithm{.build_left = join.build_left,
         .left                                = left,
         .right                               = right,
-        .results                             = results,
+        .results                             = {},
         .left_col                            = join.left_attr,
         .right_col                           = join.right_attr,
         .output_attrs                        = output_attrs};
@@ -175,66 +185,98 @@ SimpleColumnarTable execute_hash_join(const Plan&          plan,
         }
     }
 
-    return results;
+    return std::make_shared<SimpleColumnarTable>(std::move(join_algorithm.results));
 }
 
-SimpleColumnarTable execute_scan(const Plan& plan,
+SimpleColumnarTableView execute_scan(const Plan& plan,
+    WorkContext* context,
     const ScanNode& scan,
     const std::vector<std::tuple<size_t, DataType>>& output_attrs) {
+    // auto table_id = scan.base_table_id;
+    // auto& table = plan.inputs[table_id];
+    // SimpleColumnarTable results;
+
+    // // Create columns with the output types
+    // for (auto [_, type] : output_attrs) {
+    //     results.add_column(type);
+    // }
+    // // Set the row count
+    // results.set_row_count(table.num_rows);
+
+    // // Fill the columns with data using iterators
+    // size_t i = 0;
+    // for (auto [col_idx, _] : output_attrs) {
+    //     auto& simple_col = results.get_column(i);
+    //     simple_col.values.reserve(table.num_rows);
+
+    //     for (const auto& value : iterate(table.columns[col_idx])) {
+    //         simple_col.values.push_back(value);
+    //     }
+    //     ++i;
+    // }
+
+    // return SimpleColumnarTableView(std::make_shared<SimpleColumnarTable>(std::move(results)));
+
     auto table_id = scan.base_table_id;
-    auto& table = plan.inputs[table_id];
-    SimpleColumnarTable results;
-
-    // Create columns with the output types
-    for (auto [_, type] : output_attrs) {
-        results.add_column(type);
-    }
-    // Set the row count
-    results.set_row_count(table.num_rows);
-
-    // Fill the columns with data using iterators
-    size_t i = 0;
-    for (auto [col_idx, _] : output_attrs) {
-        auto& simple_col = results.get_column(i);
-        simple_col.values.reserve(table.num_rows);
-
-        for (const auto& value : iterate(table.columns[col_idx])) {
-            simple_col.values.push_back(value);
-        }
-        ++i;
-    }
-
-    return results;
+    namespace views = ranges::views;
+    auto view_indices = output_attrs
+        | views::transform([](const auto& v) { return std::get<0>(v); })
+        | ranges::to<std::vector<size_t>>();
+    return SimpleColumnarTableView(context->tables[table_id], view_indices);
 }
 
-SimpleColumnarTable execute_impl(const Plan& plan, size_t node_idx) {
+SimpleColumnarTableView execute_impl(const Plan& plan, WorkContext* context, size_t node_idx) {
     auto& node = plan.nodes[node_idx];
     return std::visit(
         [&](const auto& value) {
             using T = std::decay_t<decltype(value)>;
             if constexpr (std::is_same_v<T, JoinNode>) {
-                return execute_hash_join(plan, value, node.output_attrs);
+                return execute_hash_join(plan, context, value, node.output_attrs);
             } else {
-                return execute_scan(plan, value, node.output_attrs);
+                return execute_scan(plan, context, value, node.output_attrs);
             }
         },
         node.data);
 }
 
-ColumnarTable execute(const Plan& plan, [[maybe_unused]] void* context) {
-    namespace views = ranges::views;
-    auto ret        = execute_impl(plan, plan.root);
-    auto ret_types  = plan.nodes[plan.root].output_attrs
-                   | views::transform([](const auto& v) { return std::get<1>(v); })
-                   | ranges::to<std::vector<DataType>>();
+void prepare(const Plan& plan, void* context) {
+    WorkContext* work_context = static_cast<WorkContext*>(context);
+    work_context->tables.clear();
+    for (size_t table_id = 0; table_id < plan.inputs.size(); ++table_id) {
+        const auto& table = plan.inputs[table_id];
+        SimpleColumnarTable results;
 
+        for (const auto& column : table.columns) {
+            results.add_column(column.type);
+        }
+        results.set_row_count(table.num_rows);
+
+        size_t i = 0;
+        for (const auto& column : table.columns) {
+            auto& simple_col = results.get_column(i);
+            simple_col.values.reserve(table.num_rows);
+
+            for (const auto& value : iterate(column)) {
+                simple_col.values.push_back(value);
+            }
+            ++i;
+        }
+        work_context->tables.emplace_back(
+            std::make_shared<SimpleColumnarTable>(std::move(results)));
+    }
+}
+
+ColumnarTable execute(const Plan& plan, void* context) {
+    auto ret = execute_impl(plan, static_cast<WorkContext*>(context), plan.root);
     return ret.to_columnar();
 }
 
 void* build_context() {
-    return nullptr;
+    return static_cast<void*>(new WorkContext());
 }
 
-void destroy_context([[maybe_unused]] void* context) {}
+void destroy_context(void* context) {
+    delete static_cast<WorkContext*>(context);
+}
 
 } // namespace Contest
